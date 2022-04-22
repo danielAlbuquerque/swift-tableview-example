34a263da7c9427af224abaf6406515fe8a4e0f42
Daniel Albuquerque <danielalbuquerque@MacBook-Pro-de-Daniel.local>
2022-04-22 19:20:39 -0300

    initial

--- SignosApp/ViewController.swift
+++ SignosApp/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var signos: [String] = [];
+    var signosSignificados: [String] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        signos.append("Libra")
+        signos.append("Capricornio")
+        
+        signosSignificados.append("Libriano")
+        signosSignificados.append("Caprica")
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return signos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "celulaReuso", for: indexPath)
+        celula.textLabel?.text = signos[indexPath.row]
+        return celula
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let alertController = UIAlertController(title: "Significado do signo", message: signosSignificados[indexPath.row], preferredStyle: .alert)
+        
+        let confirmAction =  UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alertController.addAction(confirmAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 
 
